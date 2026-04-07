ad904704c135bd6dd97aa006bea8dd4750ee053c
Manjari <manjari@Manjari.localdomain>
2026-04-07 16:27:48 +0300

    created an object containing user info and initialized an outer function

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-
-
-
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+// write code that allows usrs to attempt login upto three times and locks account after three failed attempts
+// module.exports = {
+//   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+// };
+// object containing git
+const userInfo ={
+  username : 'user1',
+  password : 'pass123',
 };
+
+function createLoginTracker(userInfo) {
+  
+}
