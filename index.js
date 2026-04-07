a81109f13e91af14f98fbe88032f681513be1c16
Manjari <manjari@Manjari.localdomain>
2026-04-07 16:51:29 +0300

    added if statements to check password attempts number and if output is correct

--- index.js
+++ index.js
@@ -9,9 +9,23 @@ const userInfo ={
 };
 
 function createLoginTracker(userInfo) {
+  // code snippet stores the number of attempts in loggin in using an arrow function
   let attemptCount = () => {
     if (password !== userInfo.password){
       attemptcount++;
+    }
+  }
+  // created an anonymus function that checks if password is correct and the number of attempts
+  const passwordAttempt = function (){
+    password = prompt('Enter User Password'); // prompts the user to enter a password
+    // checks if password is correct and attempts are less than three and provides output if both are correct
+    if(password === userInfo.password && attemptCount <=3){
+      return "login Successful";
+    }
+      // checks if password is incorrect and attempts are less than three and provides output if both are correct
+    else if (password !== userInfo.password && attemptCount <= 3){
+      return `Log in attempt : ${attemptcount} , Login Failed!`
+     
     }
   }
 }
