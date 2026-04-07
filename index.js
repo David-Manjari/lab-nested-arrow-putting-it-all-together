2fe6a009369a64648a12cdb4b329c30919621ef5
Manjari <manjari@Manjari.localdomain>
2026-04-07 16:57:03 +0300

    encapsulated checking password in if statement that checks attempt count and locks account if attempt is more than 3

--- index.js
+++ index.js
@@ -15,17 +15,27 @@ function createLoginTracker(userInfo) {
       attemptcount++;
     }
   }
-  // created an anonymus function that checks if password is correct and the number of attempts
-  const passwordAttempt = function (){
-    password = prompt('Enter User Password'); // prompts the user to enter a password
-    // checks if password is correct and attempts are less than three and provides output if both are correct
-    if(password === userInfo.password && attemptCount <=3){
-      return "login Successful";
-    }
+  // created an if statement to check if password attempts are more than three
+  if( passwordAttempt <= 3){
+    // created an anonymus function that checks if password is correct and the number of attempts
+
+    const passwordAttempt = function () {
+      password = prompt('Enter User Password'); // prompts the user to enter a password
+      // checks if password is correct and attempts are less than three and provides output if both are correct
+      if (password === userInfo.password && attemptCount <= 3) {
+        return "login Successful";
+      }
       // checks if password is incorrect and attempts are less than three and provides output if both are correct
-    else if (password !== userInfo.password && attemptCount <= 3){
-      return `Log in attempt : ${attemptcount} , Login Failed!`
-     
+      else if (password !== userInfo.password && attemptCount <= 3) {
+        console.log(`Log in attempt : ${attemptcount} , Login Failed!`)
+        return prompt('Try entering password again : ')
+
+      }
     }
   }
+  else{
+    return 'Account locked due to too many failed login attempts'
+  }
+  
+
 }
