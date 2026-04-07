60086a3e1c2edbc01fff174e14fd88f3e483e79d
Manjari <manjari@Manjari.localdomain>
2026-04-07 18:13:17 +0300

    removed anonymus function, changed initialization of parent funtion and scope of password

--- index.js
+++ index.js
@@ -7,40 +7,43 @@ const userInfo ={
   username : 'user1',
   password : 'pass123',
 };
-let attemptCount = 0;
-function createLoginTracker(userInfo,password) {
+
+function createLoginTracker(userInfo) {
+ const password = prompt("Enter account password");
   // code snippet stores the number of attempts in loggin in using an arrow function
   console.log("event1")
-  
+  let attemptCount = 0;
   const checkAttempt = (userInfo,password) => {
     if (password !== userInfo.password){
       attemptCount++;
-      return attemptCount;
     }
     
   }
   console.log(attemptCount)
-  // created an if statement to check if password attempts are more than three
+  
+    // created an if statement to check if password attempts are more than three
   if( attemptCount < 3){
-    // created an anonymus function that checks if password is correct and the number of attempts
-    console.log("umbrella succ")
-    const passwordAttempt = function (userInfo,password,attemptCount) {
+    
+    
       // checks if password is correct and attempts are less than three and provides output if both are correct
-      if (password === userInfo.password && attemptCount <= 3) {
+      if (password === userInfo.password) {
         return "login Successful";
       }
       // checks if password is incorrect and attempts are less than three and provides output if both are correct
-      if(attemptCount<3)  {
+      else  {
         console.log(`Log in attempt : ${attemptCount} , Login Failed!`)
-        checkAttempt()
+        checkAttempt(userInfo,password)
         return prompt('Try entering password again : ')
 
       }
     }
-  }
-  else{
+  else {
     return 'Account locked due to too many failed login attempts'
   }
+  
+
+ 
 }
 
-let password = createLoginTracker(prompt("Enter account password"))
+const result = createLoginTracker(userInfo)
+console.log(result)
