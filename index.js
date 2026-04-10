76d243e944eaa7983a0a6c331dbed2253e1e9332
Manjari <manjari@Manjari.localdomain>
2026-04-10 21:23:00 +0300

    Created an object holding user info and a parent function

--- index.js
+++ index.js
@@ -1,48 +1,9 @@
-// write code that allows usrs to attempt login upto three times and locks account after three failed attempts
-// module.exports = {
-//   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-// };
-// object containing git
+// create a function log in tracker with a parameter userInfo as an object
+
 const userInfo ={
-  username : 'user1',
-  password : 'pass123',
-};
-
+  username : "User1",
+  password : "pass123"
+}
 function createLoginTracker(userInfo) {
-let password = prompt("Enter account password");
-  // code snippet stores the number of attempts in loggin in using an arrow function
-  let attemptCount = 1;
-  const checkAttempt = (userInfo,password) => {
-    if (password !== userInfo.password){
-      attemptCount++;
-    }
-    
-  }
-  
-    // created an if statement to check if password attempts are more than three
-  for(attemptCount;attemptCount < 3;){
-    if (attemptCount < 3) {
-
-
-      // checks if password is correct and attempts are less than three and provides output if both are correct
-      if (password === userInfo.password) {   return "login Successful";
-        
-        
-      }
-      while (password !== userInfo.password && attemptCount < 3) {
-        password = prompt('Try entering password again : ')
-        console.log(`Log in attempt : ${attemptCount} , Login Failed!`)
-        checkAttempt(userInfo, password) 
-        
-        
-      }
-      return "Account locked due to too many failed login attempts";
-    }
-    
-
-  }
   
 }
-
-const result = createLoginTracker(userInfo)
-console.log(result)
